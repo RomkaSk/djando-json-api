de0065276f29834396e6746db45ae68ac8c4ce2d
Roman Kravets <romka.spike@gmail.com>
2018-01-02 15:51:10 +0200

    new structure

--- nash/api/urls.py
+++ nash/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from example import views
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -12,5 +12,5 @@ router.register(r'entrys', views.EntrysViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+  
 ]
